0f081d2cb0a4da3ebbe2b1571099b01ee8453023
lida-belis <bellida88@mail.ru>
2020-06-18 14:06:59 +0300

    Add CheckoutOverviewPage, add FinishPage

---
+++ src/test/java/pageObject/pages/CheckoutOverviewPage.java
@@ -0,0 +1,85 @@
+package pageObject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CheckoutOverviewPage {
+    private WebDriver driver;
+
+    private By CHECKOUTOVERVIEWSELECTOR = By.className("subheader");
+    private By FINISHBUTTONSELECTOR = By.className("btn_action cart_button");
+    private By CANCELBUTTONSELECTOR = By.className("cart_cancel_link btn_secondary");
+    private By PAYMENTINFORMATIONSELECTOR = By.xpath("//div[text()='SauceCard #31337']");
+    private By SHIPPINGINFORMATIONSELECTOR = By.xpath("//div[text()='FREE PONY EXPRESS DELIVERY!']");
+    private By ITEMTOTALSELECTOR = By.className("summary_subtotal_label");
+    private By TAXSELECTOR = By.className("summary_tax_label");
+    private By TOTALSELECTOR = By.className("summary_total_label");
+
+
+    private String itemIdentificator = "//div[text()='replace']/ancestor::div[@class='cart_item']";
+    private String itemNameIdentificator = itemIdentificator + "/descendant::div[@class='inventory_item_name']";
+    private String itemPriceIdentificator = itemIdentificator + "/descendant::div[@class='inventory_item_price']";
+    private String itemDescriptionIdentificator = itemIdentificator + "/descendant::div[@class='inventory_item_desc']";
+
+    public CheckoutOverviewPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isPageOpened() {
+        return driver.findElement(CHECKOUTOVERVIEWSELECTOR).isDisplayed();
+    }
+
+    public String getName(String name) {
+        String tmpItemNameIdenficator = itemNameIdentificator.replace("replace", name);
+        WebElement itemName = driver.findElement(By.xpath(tmpItemNameIdenficator));
+        return itemName.getText();
+    }
+
+    public String getPrice(String name) {
+        String tmpItemPriceIdentificator = itemPriceIdentificator.replace("replace", name);
+        WebElement price = driver.findElement(By.xpath(tmpItemPriceIdentificator));
+        return price.getText();
+    }
+
+    public String getDescription(String name) {
+        String tmpItemDescriptionIdentificator = itemDescriptionIdentificator.replace("replace", name);
+        WebElement description = driver.findElement(By.xpath(tmpItemDescriptionIdentificator));
+        return description.getText();
+    }
+
+    public void finish() {
+        WebElement finishButton = driver.findElement(FINISHBUTTONSELECTOR);
+        finishButton.click();
+    }
+
+    public void cancel() {
+        WebElement cancelButton = driver.findElement(CANCELBUTTONSELECTOR);
+        cancelButton.click();
+    }
+
+    public String getPaymentInformation() {
+        WebElement paymentInformation = driver.findElement(PAYMENTINFORMATIONSELECTOR);
+        return paymentInformation.getText();
+    }
+
+    public String getShippingInformation() {
+        WebElement shippingInformation = driver.findElement(SHIPPINGINFORMATIONSELECTOR);
+        return shippingInformation.getText();
+    }
+
+    public String getItemTotal() {
+        WebElement itemTotal = driver.findElement(ITEMTOTALSELECTOR);
+        return itemTotal.getText().replace("Item total: $", "");
+    }
+
+    public String getTax() {
+        WebElement itemTax = driver.findElement(TAXSELECTOR);
+        return itemTax.getText().replace("Tax: $", "");
+    }
+
+    public String getTotal() {
+        WebElement total = driver.findElement(TOTALSELECTOR);
+        return total.getText().replace("Total: $", "");
+    }
+}
---
+++ src/test/java/pageObject/pages/FinishPage.java
@@ -0,0 +1,31 @@
+package pageObject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FinishPage {
+    private WebDriver driver;
+
+    private By FINISHSELECTOR = By.className("subheader");
+    private By THANKYOUFORYOURORDERSELECTOR = By.className("complete-header");
+    private By YOURORDERHASBEENDISPATCHED = By.className("complete-text");
+
+    public FinishPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isPageOpened() {
+        return driver.findElement(FINISHSELECTOR).isDisplayed();
+    }
+
+    public String getThankYouForYourOrder() {
+        WebElement thankYou = driver.findElement(THANKYOUFORYOURORDERSELECTOR);
+        return thankYou.getText();
+    }
+
+    public String getYourOrderHasBeenDispatched() {
+        WebElement YourOrderHasBeenDispatched = driver.findElement(YOURORDERHASBEENDISPATCHED);
+        return YourOrderHasBeenDispatched.getText();
+    }
+}
