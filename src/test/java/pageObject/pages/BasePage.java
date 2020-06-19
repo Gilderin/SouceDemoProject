32bded7993e5cbbe747042177333098829869986
Gilderin <hotabi4-96@yandex.ru>
2020-06-19 20:33:20 +0300

    Revert "refactor project"
    This reverts commit 844d7f3f

--- src/test/java/pageObject/pages/BasePage.java
+++ src/test/java/pageObject/pages/BasePage.java
@@ -2,13 +2,13 @@ package pageObject.pages;
 
 import driverSettings.BrowserService;
 import org.openqa.selenium.WebDriver;
-import pageObject.tests.BaseTests;
 
-public class BasePage extends BaseTests {
+public class BasePage extends BrowserService {
     WebDriver driver;
+    BrowserService browserService = new BrowserService();
 
     public BasePage(WebDriver driver) {
-        this.driver = driver;
+        this.driver = browserService.initBrowser();
     }
 
 }
